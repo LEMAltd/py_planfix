401c35c38ef3cb8decb57bcf18c35f566c881ccb
Дмитрий Федин <d.fedin@hunterhelp.ru>
2017-03-23 13:33:54 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='py_planfix',
+    version='0.0.1',
+    packages=['py_planfix'],
+    url='',
+    license='',
+    author='zloiia',
+    author_email='zloiiaarhpgu@gmail.com',
+    description=''
+)
