49939365e7e44b5123183af9c1aad25dd3947701
Dmitrii <fedin.da@gmail.com>
2018-05-31 12:27:24 +0300

    Move to organization

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name='py_planfix',
     version='0.0.1',
     packages=['py_planfix'],
-    url='https://github.com/zloiia/py_planfix',
+    url='https://github.com/LEMAltd/py_planfix',
     license='MIT',
-    author='zloiia',
-    author_email='zloiiaarhpgu@gmail.com',
+    author='LEMAltd',
+    author_email='github@hunterhelp.biz',
     description='',
     install_requires= [
         'requests',
